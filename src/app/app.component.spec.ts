9a7a6a4ba8a6b40485938b529420d6ad355a8bd5
rajesh23-drg <rmanjunath@teamdrg.com>
2018-12-21 18:45:39 +0530

    making app work

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-angular'`, () => {
+  it(`should have as title 'todo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-angular');
+    expect(app.title).toEqual('todo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo!');
   });
 });
