78588189e9d9d620a7725a790a2f4ea3e31c902b
rajesh23-drg <rmanjunath@teamdrg.com>
2018-12-21 20:52:20 +0530

    trying auto deployment

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,19 +1,23 @@
-import { HardcodedAuthenticationService } from './../service/hardcoded-authentication.service';
-import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from "./../service/hardcoded-authentication.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"]
 })
 export class MenuComponent implements OnInit {
   //isUserLoggedIn: boolean = false;
 
-  constructor(private hardcodedAuthenticationService 
-    : HardcodedAuthenticationService) { }
+  constructor(
+    private hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {}
 
   ngOnInit() {
     //this.isUserLoggedIn = this.hardcodedAuthenticationService.isUserLoggedIn();
   }
 
+  userLoggedIn() {
+    return this.hardcodedAuthenticationService.isUserLoggedIn();
+  }
 }
