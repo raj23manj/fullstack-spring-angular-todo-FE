6848029f0b4355998122cb85f6d83dc42d2dcdb9
rajesh23-drg <rmanjunath@teamdrg.com>
2018-12-29 16:56:45 +0530

    basic authentication done

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,21 +1,20 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 export class HelloWorldBean {
-  constructor(public message:string){ }
+  constructor(public message: string) {}
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class WelcomeDataService {
-
-  constructor(
-    private http:HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   executeHelloWorldBeanService() {
-    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean');
+    return this.http.get<HelloWorldBean>(
+      "http://localhost:8080/hello-world-bean"
+    );
     //console.log("Execute Hello World Bean Service")
   }
   //http://localhost:8080/hello-world/path-variable/in28minutes
@@ -24,29 +23,31 @@ export class WelcomeDataService {
     // let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
 
     // let headers = new HttpHeaders({
-    //     Authorization: basicAuthHeaderString
-    //   })
+    //   Authorization: basicAuthHeaderString
+    // });
 
     return this.http.get<HelloWorldBean>(
-      `http://localhost:8080/hello-world/path-variable/${name}`,
-      //{headers}
-      );
+      `http://localhost:8080/hello-world/path-variable/${name}`
+      //{ headers }
+    );
     //console.log("Execute Hello World Bean Service")
   }
 
+  // basic auth header, base 64 format btoa
   // createBasicAuthenticationHttpHeader() {
-  //   let username = 'in28minutes'
-  //   let password = 'dummy'
-  //   let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+  //   let username = "in28minutes";
+  //   let password = "dummy";
+  //   let basicAuthHeaderString =
+  //     "Basic " + window.btoa(username + ":" + password);
   //   return basicAuthHeaderString;
   // }
-  //Access to XMLHttpRequest at 
-  //'http://localhost:8080/hello-world/path-variable/in28minutes' 
-  //from origin 'http://localhost:4200' has been blocked by CORS policy: 
+  //Access to XMLHttpRequest at
+  //'http://localhost:8080/hello-world/path-variable/in28minutes'
+  //from origin 'http://localhost:4200' has been blocked by CORS policy:
   //No 'Access-Control-Allow-Origin' header is present on the requested resource.
 
-  //Access to XMLHttpRequest at 'http://localhost:8080/hello-world/path-variable/in28minutes' from origin 'http://localhost:4200' 
-  //has been blocked by CORS policy: 
-  //Response to preflight request doesn't pass 
+  //Access to XMLHttpRequest at 'http://localhost:8080/hello-world/path-variable/in28minutes' from origin 'http://localhost:4200'
+  //has been blocked by CORS policy:
+  //Response to preflight request doesn't pass
   //access control check: It does not have HTTP ok status
 }
--- src/app/service/http/http-intercepter-basic-auth.service.ts
+++ src/app/service/http/http-intercepter-basic-auth.service.ts
@@ -1,32 +1,35 @@
-import { BasicAuthenticationService } from './../basic-authentication.service';
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { BasicAuthenticationService } from "./../basic-authentication.service";
+import {
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class HttpIntercepterBasicAuthService implements HttpInterceptor{
+export class HttpIntercepterBasicAuthService implements HttpInterceptor {
+  constructor(private basicAuthenticationService: BasicAuthenticationService) {}
 
-  constructor(
-    private basicAuthenticationService : BasicAuthenticationService
-  ) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    // basic authentication, using manual
+    // let username = "in28minutes";
+    // let password = "dummy";
+    // let basicAuthHeaderString =
+    //   "Basic " + window.btoa(username + ":" + password);
 
-  intercept(request: HttpRequest<any>, next: HttpHandler){
-    // let username = 'in28minutes'
-    // let password = 'dummy'
-    //let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    // basic authentication autentication
     let basicAuthHeaderString = this.basicAuthenticationService.getAuthenticatedToken();
-    let username = this.basicAuthenticationService.getAuthenticatedUser()
+    let username = this.basicAuthenticationService.getAuthenticatedUser();
 
-    if(basicAuthHeaderString && username) { 
+    if (basicAuthHeaderString && username) {
       request = request.clone({
-        setHeaders : {
-            Authorization : basicAuthHeaderString
-          }
-        }) 
+        setHeaders: {
+          Authorization: basicAuthHeaderString
+        }
+      });
     }
     return next.handle(request);
   }
-
-
 }
